4f745801d0108ee97612cba5e2b23c0c6ac78eec
Irene Olmos <ireneog_72@hotmail.es>
2017-11-10 23:57:29 +0100

    creamos index en user controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -14,17 +14,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $usuarios = User::all();
+        return response()->json(['data' => $usuarios],200);
+        //return $usuarios;
     }
 
     /**
@@ -45,17 +37,6 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
